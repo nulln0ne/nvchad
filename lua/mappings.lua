9519860aa21dd5803f8df5330b8b74f84e281f7f
nulln0ne <uladzislau.hinko@gmail.com>
2025-01-30 01:22:09 +0300

    feat: add copilot

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,7 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("i", "<C-l>", function()
+  vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
+end, { desc = "Copilot Accept", noremap = true, silent = true })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -34,4 +34,13 @@ return {
       require "configs.lint"
     end,
   },
+
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+    end,
+  },
 }
